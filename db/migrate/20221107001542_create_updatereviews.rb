9a95a12d946d69636a7d315325d07c6cdaf333de
rodgersochieng <rodger.ochieng@student.moringaschool.com>
2022-11-07 03:17:04 +0300

    completed challenge

---
+++ db/migrate/20221107001542_create_updatereviews.rb
@@ -0,0 +1,7 @@
+class CreateUpdatereviews < ActiveRecord::Migration[6.1]
+  def change
+    add_column :reviews, :user_id, :integer
+    add_column :reviews, :product_id, :integer
+  end
+  end
+end
