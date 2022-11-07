5b3e6b59c32eda44445cf7fd2f029d01b18015f5
rodgersochieng <rodger.ochieng@student.moringaschool.com>
2022-11-07 03:05:30 +0300

    completed challenge

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,18 @@
 class Product < ActiveRecord::Base
+
+    has_many :reviews
+    has_many :users, through: :reviews
+
+    def leave_review(user, star_rating, comment)
+        Review.create(star_rating: star_rating, comment: comment, user_id: user.id, product_id: product.id)
+    end
+
+    def print_all_review
+        self.reviews.each(&:print_review)
+    end
+
+    def average_rating
+        self.reviews.average(:star_rating).to_f
+    end
     
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ActiveRecord::Base
+    has_many :reviews
+    has_many :products, through: :reviews
+
+    def favorite_product
+        self.reviews.order(:star_ratings).first
+    end
+
+    def remove_reviews(product)
+        self.reviews.where(product: product).destroy_all
+    end
     
 end
